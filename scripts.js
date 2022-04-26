b82e67865845eb7c9523820191ac181944597116
Alefer Reinert <aleferreinert@gmail.com>
2022-04-26 19:04:17 -0300

    Adicionado os scripts em uma função de encapsulamento.

--- scripts.js
+++ scripts.js
@@ -1,69 +1,71 @@
-"use strict";
+(function () {
+    "use strict";
 
-let hh = 0,
-    mm = 0,
-    ss = 0,
-    ms = 0,
-    time = 10,
-    cron,
-    btnStart = document.getElementById('btn-start'),
-    btnPause = document.getElementById('btn-pause'),
-    btnStop = document.getElementById('btn-stop'),
-    counter = document.getElementById('counter');
+    let hh = 0,
+        mm = 0,
+        ss = 0,
+        ms = 0,
+        time = 10,
+        cron,
+        btnStart = document.getElementById('btn-start'),
+        btnPause = document.getElementById('btn-pause'),
+        btnStop = document.getElementById('btn-stop'),
+        counter = document.getElementById('counter');
 
-function timer() {
-    ms++;
+    function timer() {
+        ms++;
 
-    if (ms == 100) {
-        ms = 0;
-        ss++;
+        if (ms == 100) {
+            ms = 0;
+            ss++;
 
-        if (ss == 60) {
-            ss = 0;
-            mm++;
+            if (ss == 60) {
+                ss = 0;
+                mm++;
 
-            if (mm == 60) {
-                mm = 0;
-                hh++;
+                if (mm == 60) {
+                    mm = 0;
+                    hh++;
+                }
             }
         }
+
+        let hhFormat = hh > 0 ? hh + ':' : '',
+            mmFormat = mm.toString().padStart(2, '0'),
+            ssFormat = ss.toString().padStart(2, '0'),
+            msFormat = ms.toString().padStart(2, '0'),
+            timeFormat = `${hhFormat}${mmFormat}:${ssFormat}<span> ${msFormat}</span>`;
+
+        counter.innerHTML = timeFormat;
+        return timeFormat;
     }
 
-    let hhFormat = hh > 0 ? hh + ':' : '',
-        mmFormat = mm.toString().padStart(2, '0'),
-        ssFormat = ss.toString().padStart(2, '0'),
-        msFormat = ms.toString().padStart(2, '0'),
-        timeFormat = `${hhFormat}${mmFormat}:${ssFormat}<span> ${msFormat}</span>`;
+    btnStart.addEventListener('click', function () {
+        cron = setInterval(() => { timer(); }, time);
+        btnStart.classList.add('hidden');
+        btnPause.classList.remove('hidden');
+        btnStop.classList.add('hidden');
+        btnStart.title = 'Continuar';
+    });
 
-    counter.innerHTML = timeFormat;
-    return timeFormat;
-}
+    btnPause.addEventListener('click', function () {
+        clearInterval(cron);
+        btnStart.classList.remove('hidden');
+        btnPause.classList.add('hidden');
+        btnStop.classList.remove('hidden');
+    });
 
-btnStart.addEventListener('click', function () {
-    cron = setInterval(() => { timer(); }, time);
-    btnStart.classList.add('hidden');
-    btnPause.classList.remove('hidden');
-    btnStop.classList.add('hidden');
-    btnStart.title = 'Continuar';
-});
+    btnStop.addEventListener('click', function () {
+        clearInterval(cron);
+        hh = 0;
+        mm = 0;
+        ss = 0;
+        ms = 0;
+        counter.innerText = '00:00:00';
 
-btnPause.addEventListener('click', function () {
-    clearInterval(cron);
-    btnStart.classList.remove('hidden');
-    btnPause.classList.add('hidden');
-    btnStop.classList.remove('hidden');
-});
-
-btnStop.addEventListener('click', function () {
-    clearInterval(cron);
-    hh = 0;
-    mm = 0;
-    ss = 0;
-    ms = 0;
-    counter.innerText = '00:00:00';
-
-    btnStart.classList.remove('hidden');
-    btnPause.classList.add('hidden');
-    btnStop.classList.add('hidden');
-    btnStart.title = 'Iniciar';
-});
+        btnStart.classList.remove('hidden');
+        btnPause.classList.add('hidden');
+        btnStop.classList.add('hidden');
+        btnStart.title = 'Iniciar';
+    });
+}());
